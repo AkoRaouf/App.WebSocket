7fa3fef15056b2bf17dc31dbfea57a7a9d4ca965
Syed Raouf Modarsi <Raouf.Modaresi@eli.com>
2020-10-10 11:08:15 +0330

    Add project files.

---
+++ App.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
